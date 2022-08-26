d684f9684d32ceba2f912ad2ebe03860e8b0c29e
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-08-26 15:52:13 +0200

    fix: ecs

--- ecs.php
+++ ecs.php
@@ -5,17 +5,16 @@ declare(strict_types=1);
 use PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer;
 use PhpCsFixer\Fixer\Phpdoc\PhpdocLineSpanFixer;
 use PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-	$services = $containerConfigurator->services();
-	$parameters = $containerConfigurator->parameters();
+return static function (ECSConfig $ecsConfig): void {
+	$services = $ecsConfig->services();
+	$ecsConfig->paths([__DIR__ . '/src']);
+	$ecsConfig->sets([SetList::CLEAN_CODE, SetList::PSR_12]);
 
-	$containerConfigurator->paths([__DIR__ . '/src']);
+	$ecsConfig->indentation('tab');
 
-	$parameters->set(Option::INDENTATION_TAB);
-	$parameters->set([SetList::CLEAN_CODE, SetList::PSR_12]);
 	$services->set(SingleQuoteFixer::class);
 	$services->set(ClassAttributesSeparationFixer::class);
 	$services->set(PhpdocLineSpanFixer::class)
